2a35c9640eead52f8e51ac7a091ba5352bde762c
bechara0 <jjbecharait@gmail.com>
2024-09-23 21:45:54 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+const title = "MI AGENDA - Lista de contactos";
+</script>
+
+<template>
+  <div>
+    <h1 class="titulo">{{ title }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.titulo {
+  color: red;
+  font-size: 18px;
+}
+</style>
